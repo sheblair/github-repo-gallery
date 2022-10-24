158894a3fb58f58b324c3a7128cabe986402d753
sheblair <66489366+sheblair@users.noreply.github.com>
2022-10-24 13:04:33 -0400

    fetch repos and display on page

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
-// this is the div to display my profile overview
 const overview = document.querySelector(".overview");
-
 const username = "sheblair";
+const repoList = document.querySelector(".repo-list");
 
 const getProfileInfo = async function () {
     const get = await fetch(`https://api.github.com/users/${username}`);
-    const response = await get.json();
-    console.log(response);
-    displayUserInfo(response);
+    const profile = await get.json();
+    displayUserInfo(profile);
 };
 
 getProfileInfo();
 
-const displayUserInfo = function (response) {
-    const userInfo = document.createElement("div");
-    userInfo.classList.add("user-info");
-    userInfo.innerHTML = 
-        `<figure><img alt="user avatar" src=${response.avatar_url} /></figure>
+const displayUserInfo = function (profile) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = 
+        `<figure><img alt="user avatar" src=${profile.avatar_url} /></figure>
         <div>
-            <p><strong>Name:</strong> ${response.name}</p>
-            <p><strong>Bio:</strong> ${response.bio}</p>
-            <p><strong>Location:</strong> ${response.location}</p>
-            <p><strong>Number of public repos:</strong> ${response.public_repos}</p>
+            <p><strong>Name:</strong> ${profile.name}</p>
+            <p><strong>Bio:</strong> ${profile.bio}</p>
+            <p><strong>Location:</strong> ${profile.location}</p>
+            <p><strong>Number of public repos:</strong> ${profile.public_repos}</p>
         </div>`
-    overview.append(userInfo);
+    overview.append(div);
+    getRepoInfo();
+};
+
+const getRepoInfo = async function () {
+    const get = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repos = await get.json();
+    console.log(repos);
+    displayRepos(repos);
+};
+
+const displayRepos = function (repos) {
+    for (let repo of repos) {
+        let li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}`
+        repoList.append(li);
+    }
 };
